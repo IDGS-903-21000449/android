30b84f98c2e80d51fa7382ac17d9d8aa51893bcd
Luis Claudio <21000449@alumnos.utleon.edu.mx>
2025-05-19 15:54:29 -0600

    primer actividad - me fallaba "actividad1"

---
+++ app/src/main/java/com/example/introkotlin903/_temasKotlin/tema6_funciones.kt
@@ -0,0 +1,13 @@
+package com.example.introkotlin903._temasKotlin
+
+fun sum(x: Int, y:Int): Int{
+    return x + y
+}
+
+fun sum2(x: Int, y:Int) = x + y
+
+fun main() {
+    println("--------------------------------------------------------------")
+    println(sum(2,3))
+    println(sum2(2,3))
+}
